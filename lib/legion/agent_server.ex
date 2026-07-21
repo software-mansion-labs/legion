ed2f72a517599bdd05cec41050ca2c1c3f6e7da6
Tomasz Idźkowski <idzkowskit@gmail.com>
2026-07-21 15:53:07 +0200

    Add parent_agent_id and started_at time to AgentRegistry

--- lib/legion/agent_server.ex
+++ lib/legion/agent_server.ex
@@ -77,7 +77,10 @@ defmodule Legion.AgentServer do
     Vault.unsafe_put(:parent_agent_id, parent_agent_id)
     if store, do: Vault.unsafe_put(:store, store)
 
-    Registry.register(Legion.AgentRegistry, agent_id, self())
+    Registry.register(Legion.AgentRegistry, agent_id, %{
+      parent_agent_id: parent_agent_id,
+      started_at: System.system_time(:millisecond)
+    })
 
     for tool <- agent_module.tools() do
       Vault.unsafe_put(tool, agent_module.tool_config(tool))
